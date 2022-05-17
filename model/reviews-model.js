d1118669eb47e354350cb13917d76b8d8876fa35
Kristinna97 <kristina.petrova13@gmail.com>
2022-05-17 11:20:09 +0100

    implement comment_count feature and test behaviour

--- model/reviews-model.js
+++ model/reviews-model.js
@@ -2,7 +2,15 @@ const db = require("../db/connection");
 
 exports.fetchReviewById = (id) => {
   return db
-    .query("SELECT * FROM reviews WHERE review_id = $1", [id])
+    .query(
+      `SELECT reviews.*,
+       CAST(COUNT(comments.comment_id) AS INTEGER) AS comment_count 
+       FROM reviews JOIN comments 
+       ON reviews.review_id = comments.review_id
+       WHERE reviews.review_id = $1 
+       GROUP BY reviews.review_id`,
+      [id]
+    )
     .then((response) => {
       if (!response.rows.length) {
         return Promise.reject({ status: 404, msg: "Not Found" });
@@ -12,9 +20,9 @@ exports.fetchReviewById = (id) => {
 };
 
 exports.updateVotes = (id, inc_votes) => {
-    if(!inc_votes){
-        return Promise.reject({ status: 400, msg: "Bad Request" });
-    }
+  if (!inc_votes) {
+    return Promise.reject({ status: 400, msg: "Bad Request" });
+  }
   return db
     .query(
       "UPDATE reviews SET votes = votes + $1 WHERE review_id = $2 RETURNING * ",
